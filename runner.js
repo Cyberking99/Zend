07a0a1595df8dc84d207035905264c8121bc3ef9
Kefas Kingsley <kefasiceking@gmail.com>
2025-12-28 03:34:58 +0100

    feat: update runner to log more data

--- runner.js
+++ runner.js
@@ -1,14 +1,12 @@
 import { execSync } from "child_process";
-import { generateCryptoChange } from "./creator.js";
+import { generateChange } from "./creator.js";
 
 const COMMITS = 260;
 
 for (let i = 1; i <= COMMITS; i++) {
-  const summary = generateCryptoChange();
-
+  const summary = generateChange();
   execSync("git add .");
   execSync(`git commit -m "${summary}"`);
-
   console.log(`✔ ${i}/${COMMITS}: ${summary}`);
 }
 
